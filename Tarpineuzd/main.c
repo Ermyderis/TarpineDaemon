66f80f5a330ec6b219a2cedfb78be3faf5caa08c
Ermyderis <71711154+Ermyderis@users.noreply.github.com>
2021-07-23 22:09:44 +0300

    Add files via upload
    fixed file openings

--- Tarpineuzd/main.c
+++ Tarpineuzd/main.c
@@ -1,11 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 #include "fileCreating.h"
 
 int main(void){
 	createLogFile("log.txt");
 	createConfigFile("config.txt");
 
-	return 0;
+
+    char buf[100];
+    getcwd(buf,100);
+    printf("%s \n",buf);
+    char config[255];
+    char logos[255];
+    //config with path
+    strcpy(config, buf);
+    strcat(config, "/config.txt");
+    printf(config);
+    printf("\n");
+    //logo with path
+    strcpy(logos, buf);
+    strcat(logos, "/log.txt");
+    printf(logos);
+    printf("\n");
+    
+
+	daemonCreated();
+
+
+	FILE *fp= NULL;
+    FILE *fpc= NULL;
+    int i = 0;
+    fpc = fopen (config, "a");
+    fprintf(fpc, "writed");
+    fflush(fpc);
+    fclose(fpc);
+
+    while (i < 10)
+    {
+        char ch;
+        fp = fopen (logos, "a");
+        fprintf(fp, "\n");
+        fflush(fp);
+        sleep(5);
+        fprintf(fp, "%d", i);
+        fflush(fp);
+        i++;
+
+        fpc = fopen (config, "r");
+        while((ch = fgetc(fpc)) != EOF){
+            fprintf(fp, "%c", ch);
+            fflush(fp);
+        }
+        fclose(fpc);
+
+
+        fclose(fp);
+    }
+
+    return (0);
 }
